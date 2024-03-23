511ba7d94a23a8cbbed991e7f35767d78bb93200
ahamada321 <a.hamada.biz@gmail.com>
2024-03-23 17:52:53 +0900

    In the middle of development

--- src/app/statics/statics.module.ts
+++ src/app/statics/statics.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { FaqComponent } from './faq/faq.component';
 import { Page404Component } from './page404/page404.component';
 import { TermsComponent } from './terms/terms.component';
 import { TermsTextModule } from './terms/helpers/terms-text/terms-text.module';
@@ -12,17 +11,11 @@ import { TutorialComponent } from './tutorial/tutorial.component';
 const routes: Routes = [
   { path: 'terms', component: TermsComponent },
   { path: 'tutorial', component: TutorialComponent },
-  { path: 'faq', component: FaqComponent },
   { path: '**', component: Page404Component },
 ];
 
 @NgModule({
-  declarations: [
-    FaqComponent,
-    Page404Component,
-    TermsComponent,
-    TutorialComponent,
-  ],
+  declarations: [Page404Component, TermsComponent, TutorialComponent],
   exports: [],
   providers: [],
   imports: [
