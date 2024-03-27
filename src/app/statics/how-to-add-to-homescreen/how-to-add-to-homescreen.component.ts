f49beb30cc64d8ea2097e0d47c99442fc6262716
Mai <manananahashi@gmail.com>
2024-03-27 11:40:53 +0900

    how to add to Home Screen page

---
+++ src/app/statics/how-to-add-to-homescreen/how-to-add-to-homescreen.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
+import { MyOriginAuthService } from 'src/app/auth/shared/auth.service';
+
+@Component({
+  selector: 'app-how-to-add-to-homescreen',
+  templateUrl: './how-to-add-to-homescreen.component.html',
+  styleUrls: ['./how-to-add-to-homescreen.component.scss'],
+})
+export class HowToAddToHomescreenComponent implements OnInit {
+  title: string = '修正さんに簡単にアクセスする';
+  description: string =
+    '「修正さん」に簡単にアクセスするにはどうすればいいの？という方向けにホーム画面に追加する方法をご紹介！';
+
+  constructor(
+    private titleService: Title,
+    private meta: Meta,
+    public auth: MyOriginAuthService
+  ) {}
+
+  ngOnInit() {
+    this.updateTitleAndMeta();
+  }
+
+  updateTitleAndMeta() {
+    this.titleService.setTitle('修正さん | ' + this.title);
+
+    this.meta.updateTag({
+      name: 'description',
+      content: this.description,
+    });
+    this.meta.updateTag({
+      property: 'og:description',
+      content: this.description,
+    });
+  }
+}
